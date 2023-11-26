5498730b5691908b0f30a0276584978a68d3e56c
Алакпаров Ибрагим Эхтият <Alakp_mr11@itstep.edu.az>
2023-11-26 09:55:11 +0400

    'Problem'

--- problem.js
+++ problem.js
@@ -1,15 +1,103 @@
-function twoSum(nums, target) {
-    for (let i = 0; i < nums.length; ++i){
-        for (let j = i + 1; j < nums.length; ++j){
-            if (nums[i] + nums[j] !== target){
-                continue;
-            } else if (nums[i] + nums[j] === target) {
-                return result = [i, j];
-            }
-        }
-    }
-};
-
 console.clear();
 
-console.log(twoSum([2, 7, 11, 15], 26));
+// class Car {
+//     beep(){
+//         return "beep";
+//     }
+// }
+
+// class Audi extends Car{
+//     // constructor(){
+//     //     super();
+//     // }
+    
+// }
+
+
+// function Car (){}
+
+// Car.prototype.beep = function(){
+//     return "beep"
+// }
+
+// function Audi (){
+//     Car.call(this);
+// }
+
+// Object.setPrototypeOf(Audi.prototype, Car.prototype);
+
+
+// const audiR8 = new Audi();
+
+// console.log(audiR8.beep());
+
+
+// class Rect {
+//     #width;
+//     #height;
+
+//     constructor (width, height){
+//         this.#width = width;
+//         this.#height = height;
+//     }
+
+//     get width (){
+//         return this.#width
+//     }
+
+//     get height (){
+//         return this.#height
+//     }
+
+//     area() {
+//         return this.#height * this.#width;
+//     }
+// }
+
+// class Square extends Rect {
+//     constructor (width, height){
+//         super(width, height)
+//     }
+
+//     perimeter(){
+//         return (this.width + this.height) * 2;
+//     }
+// }
+
+function Rect(width, height) {
+    const _width = width;
+    const _height = height;
+
+    Object.defineProperty(this, "width", {
+        get() {
+            return _width;
+        }
+    });
+
+    Object.defineProperty(this, "height", {
+        get() {
+            return _height;
+        }
+    });
+}
+
+Rect.prototype.area = function () {
+    return this.width * this.height;
+}
+
+function Square(side){
+    Rect.call(this, side, side);
+}
+
+Object.setPrototypeOf(Square.prototype, Rect.prototype);
+
+Square.prototype.perimeter = function (){
+    return (this.width + this.height) * 2;
+}
+
+const square1 = new Rect(12, 10);
+
+const square2 = new Square(3, 4);
+
+console.log(square2.area());
+console.log(square2.perimeter());
